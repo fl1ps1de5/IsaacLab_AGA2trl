klespr back with fixed variance for stochasticity

source/standalone/klespr_main/agda_run.py
@@ -114,8 +114,7 @@ def monkeypatched_act(self, inputs, role):
         exploration_tensor = torch.zeros_like(log_std) + fixed_std
 
         # "sample actions" from distribution with fixed variance
-        # comment out exploration to create deterministic actions
-        actions = mean_actions  #  + epsilon * exploration_tensor
+        actions = mean_actions + epsilon * exploration_tensor
 
         # clip actions
         if self._clip_actions: