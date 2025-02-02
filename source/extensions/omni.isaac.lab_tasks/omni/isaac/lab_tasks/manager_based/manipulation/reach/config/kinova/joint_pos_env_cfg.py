45374b79532aa7c20e028a9b6c96f93767682dad
fl1ps1de5 <ethanhirschowitz@protonmail.com>
2025-02-02 14:26:40 +1100

    kinova training at more stable state - still does not meet franka benchmark but training is stable

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/manipulation/reach/config/kinova/joint_pos_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/manipulation/reach/config/kinova/joint_pos_env_cfg.py
@@ -30,23 +30,19 @@ class KinovaReachEnvCfg(ReachEnvCfg):
         # switch robot to Kinova
         self.scene.robot = KINOVA_JACO2_N6S300_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
 
-        # simple setup for debugging
-        # override scene to have one environment
-        self.scene.num_envs = 1
-        # override observations to disable corruption
-        self.observations.policy.enable_corruption = False
+        # override events to bear in mind inital joint positions
+        self.events.reset_robot_joints.params["position_range"] = (0.9, 1.1)
 
         # override rewards
-        # note "gripper" not working so replacing with "j2n6s300_end_effector"
         self.rewards.end_effector_position_tracking.params["asset_cfg"].body_names = ["j2n6s300_end_effector"]
         self.rewards.end_effector_position_tracking_fine_grained.params["asset_cfg"].body_names = [
             "j2n6s300_end_effector"
         ]
         self.rewards.end_effector_orientation_tracking.params["asset_cfg"].body_names = ["j2n6s300_end_effector"]
 
-        # override actions
+        # override actions and adjust scale for kinova
         self.actions.arm_action = mdp.JointPositionActionCfg(
-            asset_name="robot", joint_names=["j2n6s300_joint_[1-6]"], scale=0.5, use_default_offset=True
+            asset_name="robot", joint_names=["j2n6s300_joint_[1-6]"], scale=0.2, use_default_offset=True
         )
 
         # override command generator body
